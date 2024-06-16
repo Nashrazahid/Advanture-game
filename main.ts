4657fef677a43f57f7f5820c0ccb844963df4260
nashrazahid <nashrazahid5588@gmail.com>
2024-06-16 07:26:49 +0500

    created

---
+++ main.ts
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+console.log("advanture game")
+
+class Player {
+    name:string;
+    fuel:number = 100;
+ 
+constructor(name:string){
+    this.name = name
+}
+decreaseHealth(){
+    this.fuel -= 25
+}
+increaseHealth(){
+    this.fuel += 30
+    if (this.fuel > 100){
+        this.fuel=100}
+}
+}
+
+class enemy{
+    name:string;
+    fuel:number=100;
+constructor(name:string){
+    this.name = name;
+}
+decreaseHealth(){
+    this.fuel -= 30
+}
+increaseHealth(){
+    this.fuel = 100
+}
+}
+
+const answer = await inquirer.prompt([
+    {
+    name:"name",
+    type :"input",
+    message:"player name:",
+    
+},
+
+{
+    name:"enemies",
+    type:"list",
+    message:"select enemie to fight",
+    choices:["zombie","sekeleton","worrier","assasian"]
+}])
+let player = new Player (answer.name)
+let enemies= new enemy (answer.enemies)
+console.log(chalk.bold.yellowBright(`${answer.name} vs ${answer.enemies}`))
+
+let condition = true;
+while(condition){
+    let option = await inquirer.prompt({
+        name : "action",
+        type : "list",
+        message:"select action",
+        choices:["attack","health potion","run"]
+})
+switch(option.action){
+    case  "attack":
+    let num = Math.floor(Math.random() * 2)
+    if(num > 0){
+    player.decreaseHealth()
+    console.log(chalk.bold.blueBright(`${answer.name} fuel is ${player.fuel}`))
+    console.log(chalk.bold.bgBlue(`${answer.enemies} fuel is ${enemies.fuel}`))
+    if (player.fuel <= 0){
+        console.log(chalk.bold.redBright(`${answer.enemies} has dead ! game over`))
+        condition = false
+    }
+    }
+    else if(num <= 0){
+    enemies.decreaseHealth()
+    console.log(chalk.bold.blueBright(`${answer.name} fuel is ${player.fuel}`))
+    console.log(chalk.bold.bgBlue(`${answer.enemies} fuel is ${enemies.fuel}`))
+    if (player.fuel <= 0){
+        console.log(chalk.bold.red(`${answer.enemies} has dead ! game over`))
+        condition = false
+    }
+    }
+    break;
+        case "health potion":
+        player.increaseHealth()
+        console.log(chalk.italic.yellow(`${answer.name} used a health potion`))
+        console.log(chalk.bold.green(`${answer.name} fuel is ${player.fuel}`))
+        
+     break;
+        case "run":
+        console.log(chalk.bold.redBright(`${answer.name} run away ......`))
+    condition = false
+    break;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
